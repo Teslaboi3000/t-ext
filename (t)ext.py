d8c608034e1ad7f26b85430cd9004cba0e92723c
Teslaboi_3000 <matteo.caputo2014@gmail.com>
2024-02-21 22:37:30 +0100

    added to option to name your file

--- (t)ext.py
+++ (t)ext.py
@@ -1,6 +1,7 @@
 func_choice = input('Welcome to (t)ext! Choose the operation you wanna do: (1)Write in a New File > ')
 if func_choice == '1':
     extension = input('What exentsion should your file have? > ')
-    file = open(f'test.{extension}', 'w')
+    file_name = input('What name should your file have? > ')
+    file = open(f'{file_name}.{extension}', 'w')
     content_of_file = input('> ')
     print(content_of_file, file=file)
